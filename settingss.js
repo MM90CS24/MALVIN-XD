68f2289bcc6a9733a1efc97e60bc96097f99d62c
MM90 <158116051+MM90CS24@users.noreply.github.com>
2025-07-29 04:52:23 -0400

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~AqdVxTga#rIhaxQTlRM69uHrFhOeWP_xB8dlqsAnTvBj8yUjqJ4E",  // add your Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "5356857364", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
